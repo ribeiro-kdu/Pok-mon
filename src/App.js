74413e560d4f21c22b46eac72037b6611b406d0e
Ricardo Barbosa <ribeiro.kdu@gmail.com>
2023-02-18 11:13:41 -0300

    pokeapi challenge solved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get('https://pokeapi.co/api/v2/pokemon')
+    .then((response) => {
+      
+      const sortedArray = [...response.data.results];
+
+      sortedArray.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+
+      setList(sortedArray)
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {list.map((item) => (
+      <Pokemon key={item.name} data={item}/>))}
+    </>
   );
 }
+
+const Pokemon = ({ data }) => {
+  const [details, setDetails] = useState(null);
+
+  useEffect(() => {
+    axios
+    .get(data.url)
+    .then((response) => setDetails(response.data));
+  }, []);
+
+  if(details === null) {
+    return <div>-</div>
+  }
+  return <div style={{ display: 'flex', alignItems: 'center'}}>
+            <img
+              src={details.sprites.front_default}
+              style={{ width: 30, marginRight: 15 }}
+            />
+            <span>
+              <b>{details.name}</b> - EXP {details.base_experience}
+            </span>
+          </div>;
+};
 
 export default App;
